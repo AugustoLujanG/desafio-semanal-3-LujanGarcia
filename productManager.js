6848a5feb93a5bc16f80a756c430cb150c448b37
ini <ini@ini>
2023-05-08 19:38:25 -0300

    Add - Products and endpoints

--- productManager.js
+++ productManager.js
@@ -1,6 +1,5 @@
-const fs = require("fs");
-
-class ProductManager {
+import fs from "fs";
+export default class ProductManager {
   constructor(fileName) {
     this.fileName = fileName;
     this.products = [];
@@ -53,7 +52,7 @@ class ProductManager {
     // Agregar el producto al arreglo con un id autoincrementable
     const newProduct = {
       ...product,
-      id: this.products[this.products.length - 1].id + 1,
+      id: this.products.length + 1,
     };
 
     this.products.push(newProduct);
@@ -61,17 +60,15 @@ class ProductManager {
     console.log(`Producto ${newProduct.id} - ${newProduct.title} agregado`);
   }
 
-  getProducts() {
+  async getProducts() {
     return this.products;
   }
 
-  getProductById(id) {
+  async getProductById(id) {
     const product = this.products.find((product) => product.id === id);
     if (product) {
       return product;
-    } else {
-      console.log("Producto no encontrado");
-    }
+    } 
   }
 
   updateProduct(id, updatedProduct) {
@@ -106,67 +103,3 @@ class ProductManager {
     console.log(`Producto ${id} eliminado`);
   }
 }
-
-// Crear una instancia de ProductManager
-const productManager = new ProductManager("products.json");
-
-// Llamar al método getProducts recién creada la instancia, debe devolver un arreglo vacío []
-const products = productManager.getProducts();
-console.log(products);
-
-// Llamar al método addProduct con los campos especificados
-const newProduct = {
-  title: "producto prueba",
-  description: "Este es un producto prueba",
-  price: 200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
-  stock: 25,
-};
-
-productManager.addProduct(newProduct);
-
-// Verificar que el objeto se haya agregado satisfactoriamente
-const updatedProducts = productManager.getProducts();
-console.log(updatedProducts);
-
-// Llamar al método getProductById con un id existente
-const foundProduct = productManager.getProductById(1);
-console.log(foundProduct);
-
-// Llamar al método getProductById con un id que no existe
-const notFoundProduct = productManager.getProductById(10);
-console.log(notFoundProduct);
-
-// Actualizar producto y verificar que se mantiene le id
-const updatedProduct = {
-  title: "producto actualizado",
-  description: "Este es un producto actualizado",
-  price: 300,
-  thumbnail: "Imagen actualizada",
-  code: "abc123",
-  stock: 30,
-};
-
-productManager.updateProduct(1, updatedProduct);
-console.log(productManager.getProducts());
-
-// Agregar otro producto para verificar la eliminación
-const otherProduct = {
-  title: "producto prueba 2",
-  description: "Este es un otro producto prueba",
-  price: 150,
-  thumbnail: "Sin imagen tampoco",
-  code: "abc456",
-  stock: 100,
-};
-
-productManager.addProduct(otherProduct);
-
-// Eliminar producto con id existente y verificar que realmente se haya eliminado
-productManager.deleteProduct(1);
-console.log(productManager.getProducts());
-
-// Eliminar producto con id que no existe
-productManager.deleteProduct(10);
-console.log(productManager.getProducts());
